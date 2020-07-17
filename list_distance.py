3caf8f8e6310e8be4327fd50282232ccbdba0cfd
Kamil Trzcinski <k.trzcin@protonmail.com>
2020-07-17 03:16:03 +0200

    Initial src commit.

---
+++ list_distance.py
@@ -0,0 +1,56 @@
+import itertools
+import math
+import sys
+from typing import Iterable, Set, Union
+
+
+def get_ints_from_input(count: int) -> Iterable[int]:
+    '''Read a single line from input and map it to int. Expects a space seperated list of values. Raises error.'''
+    ints = map(int, input().split(' '))
+    elements: int = 0
+
+    while True:
+        try:
+            value: int = next(ints)
+            elements += 1
+            if elements > count:
+                break
+            yield value
+        except StopIteration:
+            break
+
+    if elements != count:
+        raise ValueError(
+            "Invalid input, too many or too few values provided!"
+        )
+
+
+def calculate_distance() -> int:
+    n: int
+    m: int
+    n, m = get_ints_from_input(2)
+    setA: Set[int] = set(get_ints_from_input(n))
+    setB: Set[int] = set(get_ints_from_input(m))
+
+    # return early if there's common element
+    if setA.intersection(setB):
+        return 0
+
+    minimum: Union[int, None]  = None
+    valA: int
+    valB: int
+    for valA, valB in itertools.product(setA, setB):
+        distance: int = abs(valA - valB)
+        if minimum is None or minimum > distance:
+            minimum = distance
+
+    return minimum
+
+
+if __name__ == "__main__":
+    try:
+        result = calculate_distance()
+        print(result)
+    except (ValueError, EOFError) as err:
+        print(err)
+        sys.exit(1)
---
+++ tests/test_list_distance.py
@@ -0,0 +1,82 @@
+from contextlib import contextmanager
+import unittest
+from unittest.mock import patch
+import sys
+import list_distance
+import pathlib
+
+from list_distance import calculate_distance
+
+
+PATH: str = pathlib.Path(__file__).parent.absolute() / "fixtures/"
+
+@contextmanager
+def patched_stdin(filename: str) -> None:
+    with open(PATH / filename) as fd:
+        with patch("sys.stdin", fd):
+            yield
+
+
+class TestValidInput(unittest.TestCase):
+
+    def test_valid_input1(self):
+        with patched_stdin("valid_input1.txt"):
+            self.assertEqual(calculate_distance(), 0)
+
+    def test_valid_input2(self):
+        with patched_stdin("valid_input2.txt"):
+            self.assertEqual(calculate_distance(), 5)
+
+    def test_valid_input3(self):
+         with patched_stdin("valid_input3.txt"):
+            self.assertEqual(calculate_distance(), 1)
+
+
+class TestArgsCountMismatch(unittest.TestCase):
+
+    def assertValueError(self):
+        with self.assertRaises(ValueError):
+            calculate_distance()
+
+    def test_too_few_args(self):
+        with patched_stdin("too_few_args.txt"):
+            self.assertValueError()
+    
+    def test_too_few_list_a_elements(self):
+        with patched_stdin("too_few_list_a_elements.txt"):
+            self.assertValueError()
+
+    def test_too_few_list_b_elements(self):
+        with patched_stdin("too_few_list_b_elements.txt"):
+            self.assertValueError()
+ 
+    def test_too_many_args(self):
+        with patched_stdin("too_many_args.txt"):
+            self.assertValueError()
+ 
+    def test_too_many_list_a_elements(self):
+        with patched_stdin("too_many_list_a_elements.txt"):
+            self.assertValueError()
+ 
+    def test_too_many_list_b_elements(self):
+        with patched_stdin("too_many_list_b_elements.txt"):
+            self.assertValueError()
+
+
+class MiscellaneousErrors(unittest.TestCase):
+
+    def test_unexpected_eof(self):
+        with patched_stdin("unexpected_eof.txt"):
+            with self.assertRaises(EOFError):
+                calculate_distance()
+
+    def test_char_in_args(self):
+        with patched_stdin("char_in_args.txt"):
+            with self.assertRaises(ValueError):
+                calculate_distance()
+
+    def test_unexpected_eof(self):
+        with patched_stdin("float_in_args.txt"):
+            with self.assertRaises(ValueError):
+                calculate_distance()
+
